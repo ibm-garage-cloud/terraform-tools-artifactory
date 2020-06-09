7405d973bbe003ef3c13d0fe818cb768bdb561b4
Sean Sundberg <seansund@us.ibm.com>
2020-06-09 15:51:10 -0500

    Updates stages to include artifactory stage (duh)

---
+++ test/setup/stages/stage2-artifactory.tf
@@ -0,0 +1,20 @@
+module "dev_serviceaccount_artifactory" {
+  source = "github.com/ibm-garage-cloud/terraform-cluster-serviceaccount.git?ref=v1.2.0"
+
+  cluster_type             = var.cluster_type
+  namespace                = module.dev_tools_namespace.name
+  cluster_config_file_path = module.dev_cluster.config_file_path
+  service_account_name     = "artifactory-artifactory"
+  sscs                     = ["anyuid", "privileged"]
+}
+
+module "dev_tools_artifactory" {
+  source = "github.com/ibm-garage-cloud/terraform-tools-artifactory.git"
+
+  cluster_type             = var.cluster_type
+  service_account          = module.dev_serviceaccount_artifactory.name
+  releases_namespace       = module.dev_serviceaccount_artifactory.namespace
+  cluster_ingress_hostname = module.dev_cluster.ingress_hostname
+  cluster_config_file      = module.dev_cluster.config_file_path
+  tls_secret_name          = module.dev_cluster.tls_secret_name
+}
