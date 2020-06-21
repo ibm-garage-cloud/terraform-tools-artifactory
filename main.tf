b59e61b4c500735852393d1032f726a6dae6727d
Sean Sundberg <seansund@us.ibm.com>
2020-06-21 17:15:31 -0500

    Fixes cluster_type name for sccs test

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ locals {
   service_account_config = {
     name = "artifactory-artifactory"
     createNamespace = false
-    sccs = var.cluster_type == "kuberentes" ? [] : ["anyuid", "privileged"]
+    sccs = var.cluster_type == "kubernetes" ? [] : ["anyuid", "privileged"]
   }
   artifactory_config     = {
     nameOverride = "artifactory"
