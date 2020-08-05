fdc09e2e921b20c20ca6ecdb74e5602511205955
Sean Sundberg <seansund@us.ibm.com>
2020-08-05 00:36:24 -0500

    Removes endpoint validation

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -22,20 +22,20 @@ if [[ -n "${POD_STATUSES}" ]]; then
 fi
 
 set -e
-
-if [[ "${CLUSTER_TYPE}" == "kubernetes" ]] || [[ "${CLUSTER_TYPE}" =~ iks.* ]]; then
-  ENDPOINTS=$(kubectl get ingress -n "${NAMESPACE}" -o jsonpath='{range .items[*]}{range .spec.rules[*]}{"https://"}{.host}{"\n"}{end}{end}')
-else
-  ENDPOINTS=$(kubectl get route -n "${NAMESPACE}" -o jsonpath='{range .items[*]}{"https://"}{.spec.host}{.spec.path}{"\n"}{end}')
-fi
-
-echo "Validating endpoints:\n${ENDPOINTS}"
-
-echo "${ENDPOINTS}" | while read endpoint; do
-  if [[ -n "${endpoint}" ]]; then
-    ${SCRIPT_DIR}/waitForEndpoint.sh "${endpoint}" 10 10
-  fi
-done
+#
+#if [[ "${CLUSTER_TYPE}" == "kubernetes" ]] || [[ "${CLUSTER_TYPE}" =~ iks.* ]]; then
+#  ENDPOINTS=$(kubectl get ingress -n "${NAMESPACE}" -o jsonpath='{range .items[*]}{range .spec.rules[*]}{"https://"}{.host}{"\n"}{end}{end}')
+#else
+#  ENDPOINTS=$(kubectl get route -n "${NAMESPACE}" -o jsonpath='{range .items[*]}{"https://"}{.spec.host}{.spec.path}{"\n"}{end}')
+#fi
+#
+#echo "Validating endpoints:\n${ENDPOINTS}"
+#
+#echo "${ENDPOINTS}" | while read endpoint; do
+#  if [[ -n "${endpoint}" ]]; then
+#    ${SCRIPT_DIR}/waitForEndpoint.sh "${endpoint}" 10 10
+#  fi
+#done
 
 CONFIG_URLS=$(kubectl get configmap -n "${NAMESPACE}" -l grouping=garage-cloud-native-toolkit -l app.kubernetes.io/component=tools -o json | jq '.items[].data | to_entries | select(.[].key | endswith("_URL")) | .[].value' | sed "s/\"//g")
 
