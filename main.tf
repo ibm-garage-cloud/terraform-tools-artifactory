99e193d0c5f7ac4db0e3972d5bc071c597084d05
Sean Sundberg <seansund@us.ibm.com>
2020-08-05 12:36:31 -0500

    Adds config service account to give job permission to work with secrets and configmaps

--- main.tf
+++ main.tf
@@ -15,6 +15,8 @@ locals {
   chart_name             = "artifactory"
   chart_dir              = "${local.gitops_dir}/${local.chart_name}"
   service_name           = "artifactory-artifactory"
+  sa_name                = "artifactory-artifactory"
+  config_sa_name         = "artifactory-config"
   global_config          = {
     storageClass = var.storage_class
     clusterType = var.cluster_type
@@ -22,9 +24,26 @@ locals {
     tlsSecretName = var.tls_secret_name
   }
   service_account_config = {
-    name = "artifactory-artifactory"
+    name = local.sa_name
     create = false
     sccs = ["anyuid", "privileged"]
+  }
+  config_service_account_config = {
+    name = local.config_sa_name
+    roles = [
+      {
+        apiGroups = [
+          ""
+        ]
+        resources = [
+          "secrets",
+          "configmaps"
+        ]
+        verbs = [
+          "*"
+        ]
+      }
+    ]
   }
   artifactory_config     = {
     nameOverride = "artifactory"
@@ -65,7 +84,7 @@ locals {
     }
     serviceAccount = {
       create = true
-      name = "artifactory-artifactory"
+      name = local.sa_name
     }
   }
   ocp_route_config       = {
@@ -91,6 +110,7 @@ locals {
   }
   job_config             = {
     name = "artifactory"
+    serviceAccountName = local.config_sa_name
     command = "setup-artifactory"
     secret = {
       name = "artifactory-access"
@@ -123,6 +143,7 @@ resource "local_file" "artifactory-values" {
   content  = yamlencode({
     global = local.global_config
     service-account = local.service_account_config
+    config-service-account = local.config_service_account_config
     artifactory = local.artifactory_config
     ocp-route = local.ocp_route_config
     tool-config = local.tool_config
