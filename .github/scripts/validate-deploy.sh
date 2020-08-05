2659a152c181dc6663c6291bf206de1025aa6cc0
Sean Sundberg <seansund@us.ibm.com>
2020-08-05 00:45:28 -0500

    Validate internal url in validate-deploy

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -37,6 +37,16 @@ set -e
 #  fi
 #done
 
+INTERNAL_URL=$(kubectl get secret artifactory-access -n "${NAMESPACE}" -o jsonpath='{.data.ARTIFACTORY_URL}' | base64 -D)
+
+SERVICE_URL="http://artifactory-artifactory.${NAMESPACE}"
+if [[ "${INTERNAL_URL}" =~ ${SERVICE_URL} ]]; then
+  echo "Internal url found"
+else
+  echo "Internal url not found"
+  exit 1
+fi
+
 CONFIG_URLS=$(kubectl get configmap -n "${NAMESPACE}" -l grouping=garage-cloud-native-toolkit -l app.kubernetes.io/component=tools -o json | jq '.items[].data | to_entries | select(.[].key | endswith("_URL")) | .[].value' | sed "s/\"//g")
 
 echo "${CONFIG_URLS}" | while read url; do
