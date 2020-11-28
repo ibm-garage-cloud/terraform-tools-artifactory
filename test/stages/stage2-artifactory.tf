43161ea9e79b7825ce90fe47f95606ddd41a6f2c
Sean Sundberg <seansund@us.ibm.com>
2020-11-27 23:00:18 -0600

    Updates logic for generating the module catalog (#27)
    * Updates module configuration to match current variables
    * Updates module metadata logic

--- test/stages/stage2-artifactory.tf
+++ test/stages/stage2-artifactory.tf
@@ -2,10 +2,10 @@ module "dev_tools_artifactory" {
   source = "./module"
 
   cluster_type             = module.dev_cluster.type_code
-  service_account          = "artifactory-artifactory"
-  releases_namespace       = module.dev_capture_state.namespace
   cluster_ingress_hostname = module.dev_cluster.ingress_hostname
   cluster_config_file      = module.dev_cluster.config_file_path
   tls_secret_name          = module.dev_cluster.tls_secret_name
+  releases_namespace       = module.dev_capture_state.namespace
+  service_account          = "artifactory-artifactory"
   persistence              = false
 }
