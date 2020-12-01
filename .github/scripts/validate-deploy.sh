626b5dc5947d7b11d8f3b2581b04bca0a4bbead5
Sean Sundberg <seansund@us.ibm.com>
2020-11-30 22:48:59 -0600

    Cleans up deprecated scripts and adds custom validate-deploy logic

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -2,41 +2,9 @@
 
 SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
 
-export KUBECONFIG="${SCRIPT_DIR}/.kube/config"
-
 CLUSTER_TYPE="$1"
 NAMESPACE="$2"
 NAME="$3"
-
-if [[ -z "${NAME}" ]]; then
-  NAME=$(echo "${NAMESPACE}" | sed "s/tools-//")
-fi
-
-echo "Verifying resources in ${NAMESPACE} namespace for module ${NAME}"
-
-PODS=$(kubectl get -n "${NAMESPACE}" pods -o jsonpath='{range .items[*]}{.status.phase}{": "}{.kind}{"/"}{.metadata.name}{"\n"}{end}' | grep -v "Running" | grep -v "Succeeded")
-POD_STATUSES=$(echo "${PODS}" | sed -E "s/(.*):.*/\1/g")
-if [[ -n "${POD_STATUSES}" ]]; then
-  echo "  Pods have non-success statuses: ${PODS}"
-  exit 1
-fi
-
-set -e
-
-#if [[ "${CLUSTER_TYPE}" == "kubernetes" ]] || [[ "${CLUSTER_TYPE}" =~ iks.* ]]; then
-#  ENDPOINTS=$(kubectl get ingress -n "${NAMESPACE}" -o jsonpath='{range .items[*]}{range .spec.rules[*]}{"https://"}{.host}{"\n"}{end}{end}')
-#else
-#  ENDPOINTS=$(kubectl get route -n "${NAMESPACE}" -o jsonpath='{range .items[*]}{"https://"}{.spec.host}{.spec.path}{"\n"}{end}')
-#fi
-#
-#echo "Validating endpoints:"
-#echo "${ENDPOINTS}"
-#
-#echo "${ENDPOINTS}" | while read endpoint; do
-#  if [[ -n "${endpoint}" ]]; then
-#    ${SCRIPT_DIR}/waitForEndpoint.sh "${endpoint}" 10 10
-#  fi
-#done
 
 INTERNAL_URL=$(kubectl get secret artifactory-access -n "${NAMESPACE}" -o jsonpath='{.data.ARTIFACTORY_URL}' | base64 -d)
 
@@ -61,13 +29,3 @@ if [[ -z "${ENCRYPT}" ]]; then
   echo "ENCRPYT password not set"
   exit 1
 fi
-
-if [[ "${CLUSTER_TYPE}" == "ocp4" ]]; then
-  echo "Validating consolelink"
-  if [[ $(kubectl get consolelink "toolkit-${NAME}" | wc -l) -eq 0 ]]; then
-    echo "   ConsoleLink not found"
-    exit 1
-  fi
-fi
-
-exit 0
