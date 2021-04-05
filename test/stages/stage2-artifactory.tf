efb700aaed397022976cce3a47d674d42950c939
Sean Sundberg <seansund@us.ibm.com>
2021-04-05 08:03:45 -0500

    Updates test logic with module dependencies
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-artifactory.tf
+++ test/stages/stage2-artifactory.tf
@@ -1,10 +1,10 @@
 module "dev_tools_artifactory" {
   source = "./module"
 
-  cluster_type             = module.dev_cluster.type_code
-  cluster_ingress_hostname = module.dev_cluster.ingress_hostname
+  cluster_type             = module.dev_cluster.platform.type_code
+  cluster_ingress_hostname = module.dev_cluster.platform.ingress
   cluster_config_file      = module.dev_cluster.config_file_path
-  tls_secret_name          = module.dev_cluster.tls_secret_name
+  tls_secret_name          = module.dev_cluster.platform.tls_secret
   releases_namespace       = module.dev_capture_state.namespace
   service_account          = "artifactory-artifactory"
   persistence              = false
